a76cc8fe811e4fc631360f6b66e336ede59cae33
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2025-07-30 18:00:55 +0300

    orders _ cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import SupportSection from "./Components/SupportCard/SupportSection";
 import ReviewsSection from "./Components/ReviewSection/ReviewsSection";
 import ShopPage from "../pages/ShopPage/ShopPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <ShopPage />
