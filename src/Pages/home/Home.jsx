3949d045bb7bc805acae7a32b7ed7644b46bdf16
Mostafa219 <mostafaarafat737@gmail.com>
2025-08-01 22:47:49 +0300

    fix carousol conflict

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import Products from "../../Components/products/products/Products";
 import JustIn from "../../Components/products/justIn/JustIn";
 import BestSellers from "../../Components/products/bestSellers/BestSellers";
+import CategoryCard from "../../Components/categoryCard/CategoryCard";
+import HeroSection from "../../Components/HeroSection/HeroSection";
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
+      <CategoryCard />
       <Products />
       <JustIn />
       <BestSellers />
