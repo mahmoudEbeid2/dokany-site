a517a14947cedd5342679e6939c665fa00418ad2
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2025-08-09 19:57:40 +0300

    solve a

--- src/Components/Cart/CartItem.jsx
+++ src/Components/Cart/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
-import styles from "../../pages/cart/cart.module.css";
+import styles from "../../Pages/cart/cart.module.css";
 
 function CartItem({ item, onDelete, onUpdateQuantity, isUpdating }) {
   console.log(item);
